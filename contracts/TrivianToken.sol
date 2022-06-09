ed53cf9bcd27c936593a6c4983c400eb3314b67f
Tansel Kaya <tanselkaya@yahoo.com>
2022-06-09 18:39:02 +0300

    Update TrivianToken.sol

--- contracts/TrivianToken.sol
+++ contracts/TrivianToken.sol
@@ -6,13 +6,19 @@ import "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
 import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
 
+// Trivian Token has 3 decimal digits
 uint8 constant numDecimals = 3;
 
+// Trivian Token conforms to the ERC20Burnable specification
 contract TrivianToken is ERC20, ERC20Burnable, Pausable, Ownable {
+    
+    // All tokens are minted by the constructor 
+    // and transferred to the multi-sig owner wallet
     constructor() ERC20("Trivian Token", "TRIVIA") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
-
+    
+    // Trivian Token can be paused
     function pause() public onlyOwner {
         _pause();
     }
