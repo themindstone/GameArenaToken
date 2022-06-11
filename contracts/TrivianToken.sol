eb0dab262355dbc2ef16695653fee78c113ecb4a
Tansel Kaya <tanselkaya@yahoo.com>
2022-06-11 18:20:36 +0300

    Update TrivianToken.sol

--- contracts/TrivianToken.sol
+++ contracts/TrivianToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
-import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Trivian Token has 3 decimal digits
 uint8 constant numDecimals = 3;
